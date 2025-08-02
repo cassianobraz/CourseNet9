15b4a5f69cf5db1fb9e68535fc796fc9dab498c6
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2025-08-02 11:03:26 -0400

    new project

---
+++ JwtAuthDotNet9/JwtAuthDotNet9/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using JwtAuthDotNet9.Entities;
+using JwtAuthDotNet9.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDotNet9.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    [HttpPost("register")]
+    public ActionResult<User> Register(UserDto request)
+    {
+        var hashedPassword = 
+    }
+
+}
