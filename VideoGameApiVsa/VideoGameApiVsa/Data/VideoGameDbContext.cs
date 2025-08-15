0d11362914067d5ba04e1de4abe0aeca9b66172e
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2025-08-15 17:27:14 -0400

    feat: new two projects

---
+++ VideoGameApiVsa/VideoGameApiVsa/Data/VideoGameDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameApiVsa.Entities;
+
+namespace VideoGameApiVsa.Data;
+
+public class VideoGameDbContext : DbContext
+{
+    public VideoGameDbContext(DbContextOptions<VideoGameDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<VideoGame> VideoGames { get; set; }
+}
